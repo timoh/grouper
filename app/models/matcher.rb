3d80816c3ad7527584d615695e665ff1738f6e8d
vagrant <vagrant@precise32.(none)>
2014-09-08 17:05:02 +0000

    group sizing now works in a rudimentary fashion

--- app/models/matcher.rb
+++ app/models/matcher.rb
@@ -4,4 +4,40 @@ class Matcher
 
   # Divide into groups where at least one time in common
 
+  # Group size as input
+
+  def Matcher.calculate_group_size(options = {}) #group size will default to 3-4
+
+    group_size = 0
+
+    min_size = options[:min_size] ||= 3 
+    max_size = options[:max_size] ||= 4
+    # walk_order = options[:order] ||= 'ASC'
+
+    # if walk_order == 'ASC'
+    #   while(group_size == 0)
+
+        
+
+    #   end
+    # else # DESC
+    #   while(group_size == 0)
+
+    #   end 
+    # end
+
+    min_modulo = Student.all.length % min_size
+    max_modulo = Student.all.length % max_size
+
+    if min_modulo >= min_size && min_modulo <= max_size
+      group_size = min_size
+    elsif max_modulo >= min_size && max_modulo <= max_size
+      group_size = max_size
+    else
+      raise 'We now have a situation where this algorithm cannot solve the correct group size!'
+    end
+
+    return 'Group size is '+group_size.to_s+', and there will be one group with the size of '+(Student.all.length % group_size).to_s+'.'
+  end
+ 
 end
