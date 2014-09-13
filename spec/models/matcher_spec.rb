8118b83679430d42cb4a77f851d5c174cc86e670
Timo Herttua <timo.herttua@efekta.fi>
2014-09-13 20:15:22 +0000

    primitive test harness

--- spec/models/matcher_spec.rb
+++ spec/models/matcher_spec.rb
@@ -3,12 +3,10 @@ require 'rails_helper'
 RSpec.describe Matcher, :type => :model do
 
 	it "creates feasible groups with a random algorithm" do
-		puts 'Starting random algorithm testing at '+Time.now.to_s
-
+		
 		# create a bunch of students
 		5.times { FactoryGirl.create(:student) } 
 
-		puts 'Students created at '+Time.now.to_s
 		# get started
 		scores = Array.new
 		counter = 0
@@ -24,8 +22,6 @@ RSpec.describe Matcher, :type => :model do
 			new_random_group.destroy
 			counter += 1
 		end
-
-		puts 'Checking scores now, at '+Time.now.to_s
 
 		scores.each do |score|
 			if score > record_high then record_high = score end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Student, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "prepares an array of all students that do not have a group" do
+    DatabaseCleaner.clean
+
+    19.times { FactoryGirl.create(:student) }
+    expect(Student.all.length).to equal 19
+
+    ua_students_array = Student.prepare_unassigned_students_array
+
+    ua_students_array.each do |student|
+      expect(student.group).to eq nil
+    end
+
+    expect(ua_students_array.size).to equal 19
+
+  end
+
 end
